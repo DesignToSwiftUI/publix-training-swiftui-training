882910313494562fcf1bd32417de6b804d0492f7
Craig Clayton <craig@designtoswiftui.io>
2023-10-27 14:24:37 -0400

    update json

---
+++ PublixMedia/PublixMedia/Networking/Services/BooksService.swift
@@ -0,0 +1,8 @@
+//
+//  BookService.swift
+//  PublixMedia
+//
+//  Created by Craig Clayton on 10/27/23.
+//
+
+import Foundation
---
+++ PublixMedia/PublixMedia/Networking/Services/MoviesService.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesService.swift
+//  PublixMedia
+//
+//  Created by Craig Clayton on 10/27/23.
+//
+
+import Foundation
